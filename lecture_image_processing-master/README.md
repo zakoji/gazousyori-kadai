f086c4019db634f3563d2617c96a2d023ac9aa9c
zakoji <35319644+zakoji@users.noreply.github.com>
2018-01-11 12:03:40 +0900

    kadai

---
+++ lecture_image_processing-master/README.md
@@ -0,0 +1,16 @@
+(参考)Matlab入門  
+http://www.ns.kogakuin.ac.jp/~ct13050/johogaku/2-2.matlab_getstart.pdf
+
+例えば，このようにしてレポートを作成することが可能です．  
+https://github.com/mackhasegawa/lecture_image_processing/blob/master/kadai1.md
+
+
+フリー素材
+- https://www.pakutaso.com/ 
+- フリー素材アイドル「MIKA☆RIKA」 http://mika-rika-free.jp/
+
+
+
+
+
+
---
+++ lecture_image_processing-master/kadai2.m
@@ -0,0 +1,25 @@
+% 課題２　階調数と疑似輪郭
+% ２階調，４階調，８階調の画像を生成せよ．
+% 下記はサンプルプログラムである．
+% 課題作成にあたっては「Lenna」以外の画像を用いよ．
+
+clear; % 変数のオールクリア
+
+ORG=imread('Lenna.png'); % 原画像の入力
+ORG = rgb2gray(ORG); colormap(gray); colorbar;
+imagesc(ORG); axis image; % 画像の表示
+pause; % 一時停止
+
+% ２階調画像の生成
+IMG = ORG>128;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+pause;
+
+% ４階調画像の生成
+IMG0 = ORG>64;
+IMG1 = ORG>128;
+IMG2 = ORG>192;
+IMG = IMG0 + IMG1 + IMG2;
+imagesc(IMG); colormap(gray); colorbar;  axis image;
+
+% ８階調については，各自検討してください．
